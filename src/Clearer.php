58c8b64e0f6e9f190fa687b8e44b442a649cbc98
Craig Morris <craig.michael.morris@gmail.com>
2019-08-05 16:21:24 +0100

    Add support for clearing reserved jobs, and delayed jobs on redis

--- src/Clearer.php
+++ src/Clearer.php
@@ -6,33 +6,62 @@ use Morrislaptop\LaravelQueueClear\Contracts\Clearer as ClearerContract;
 
 class Clearer implements ClearerContract
 {
-	/**
-	 * @var QueueManager
-	 */
-	protected $manager;
+    /**
+     * @var QueueManager
+     */
+    protected $manager;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	function __construct(FactoryContract $manager)
-	{
-		$this->manager = $manager;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    function __construct(FactoryContract $manager)
+    {
+        $this->manager = $manager;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function clear($connection, $queue)
-	{
-		$count = 0;
-		$connection = $this->manager->connection($connection);
+    /**
+     * {@inheritDoc}
+     */
+    public function clear($connection, $queue)
+    {
+        $count = 0;
+        $connection = $this->manager->connection($connection);
 
-		while ($job = $connection->pop($queue)) {
-			$job->delete();
-			$count++;
-		}
+        $count += $this->clearJobs($connection, $queue);
+        $count += $this->clearJobs($connection, $queue . ':reserved');
+        $count += $this->clearDelayedJobs($connection, $queue);
 
-		return $count;
-	}
+        return $count;
+    }
+
+    protected function clearJobs($connection, $queue)
+    {
+        $count = 0;
+
+        while ($job = $connection->pop($queue)) {
+            $job->delete();
+            $count++;
+        }
+
+        return $count;
+    }
+
+    protected function clearDelayedJobs($connection, $queue)
+    {
+        if (method_exists($connection, 'getRedis')) {
+            return $this->clearDelayedJobsOnRedis($connection, $queue);
+        }
+
+        throw new \InvalidArgumentException('Queue Connection not supported');
+    }
+
+    protected function clearDelayedJobsOnRedis($connection, $queue) {
+        $key = "queues:{$queue}:delayed";
+        $redis = $connection->getRedis()->connection(config('queue.connections.redis.connection'));
+        $count = $redis->zcount($key, '-inf', '+inf');
+        $redis->del($key);
+
+        return $count;
+    }
 
 }
